f12cde2d2e2c532a45fabf212b7778d823968f4e
wili1991 <wikki9106@gmail.com>
2023-02-10 20:22:20 -0500

    operador NOT

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-let a = confirm("Variable A\n\t\tSeleciona Aceptar si deseas el boleano True\n\t\tSeleciona Cancelar si deseas el boleano False");
-let b = confirm("Variable B\n\t\tSeleciona Aceptar si deseas el boleano True\n\t\tSeleciona Cancelar si deseas el boleano False");
+let a = confirm("Variable A\n\t\tSeleciona Aceptar si deseas el boleano True\n\t\tSeleciona Cancelar si deseas el boleano False")
 
-let cuerpo = document.querySelector("#myOr");
+let cuerpo = document.querySelector("#myNot");
 cuerpo.insertAdjacentHTML("beforeend", ` <tr>
                                             <td>${a}</td>
-                                            <td>${b}</td>
-                                            <td>${a || b}</td>
+                                            <td>${!a}</td>
                                         </tr>`);
-
-
