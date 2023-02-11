dae877d5ca9340dba265983f33768e2bda1a2ae2
wili1991 <wikki9106@gmail.com>
2023-02-10 20:08:06 -0500

    operador logico OR

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let a = confirm("Variable A\n\t\tSeleciona Aceptar si deseas el boleano True\n\t\tSeleciona Cancelar si deseas el boleano False");
 let b = confirm("Variable B\n\t\tSeleciona Aceptar si deseas el boleano True\n\t\tSeleciona Cancelar si deseas el boleano False");
 
-let cuerpo = document.querySelector("#myAND");
+let cuerpo = document.querySelector("#myOr");
 cuerpo.insertAdjacentHTML("beforeend", ` <tr>
                                             <td>${a}</td>
                                             <td>${b}</td>
-                                            <td>${a && b}</td>
+                                            <td>${a || b}</td>
                                         </tr>`);
 
 
